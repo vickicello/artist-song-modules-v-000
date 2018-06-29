03e5914d4b0a16fc4df56b9f94de8a0917e1c1b1
Vicki Hamm <vicki.cello@gmail.com>
2018-06-29 15:41:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,14 +5,14 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
-  
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   def artist=(artist)
